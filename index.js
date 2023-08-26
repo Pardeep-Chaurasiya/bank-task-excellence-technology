59e73ad760c6b5ae6d1a2b73e3c466be34217c34
Pardeep Chaurasiya <chaurasiyapardeep001@gmail.com>
2023-08-26 15:28:56 +0530

    solve cors error

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require("express");
 const dotenv = require("dotenv").config();
 const userRouter = require("./routes/userRoutes");
 const bodyParser = require("body-parser");
+const cors = require(cors);
 
 const app = express();
 
 const PORT = process.env.PORT;
+app.use(cors());
 app.use(express.json());
 app.use("/api/user/", userRouter);
 
