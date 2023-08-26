777b2c2cc2a4b398d7345301b9995d9b4e75edad
Pardeep Chaurasiya <chaurasiyapardeep001@gmail.com>
2023-08-26 16:10:15 +0530

    .env solved

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const cors = require("cors");
 const app = express();
 
 const PORT = process.env.PORT;
-app.use(cors());
 app.use(express.json());
+app.use(cors());
 app.use("/api/user/", userRouter);
 
 app.listen(PORT, () => {
